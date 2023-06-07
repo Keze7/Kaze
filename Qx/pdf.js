f19b431efef41dc0cdc6a42462239cede4958ead
Keze7 <127592616+Keze7@users.noreply.github.com>
2023-06-07 15:35:44 +0800

    Create pdf.js

---
+++ Qx/pdf.js
@@ -0,0 +1,43 @@
+[rewrite_local]
+
+^http[s]?:\/\/awvp\.aoscdn\.com\/base\/vip\/v2\/vips  url script-response-body https://github.com/Keze7/Kaze/raw/main/QX/pdf.js 
+
+[mitm]
+
+hostname = gw.aoscdn.com
+
+ /*
+
+‼️规则完全免费，仅供学习交流，🈲️商业用途
+
+*/
+
+var body = $response.body;
+
+var url = $request.url;
+
+var obj = JSON.parse(body);
+
+const vip = '/account/me';
+
+if (url.indexOf(vip) != -1) {
+
+    obj.data.candy = 1;
+
+obj.data.candy_expired_at = 9999999;
+
+obj.data.pending = 1;
+
+obj.data. status = 1;
+
+obj.data.quota = 1;
+
+obj.data. remained_seconds = 1;
+
+obj.data.is_tried = 1;
+
+    body = JSON.stringify(obj);
+
+}
+
+$done({body});
