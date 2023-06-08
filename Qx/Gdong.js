31ee469d974ee2fa5f021c18509d0cb90e87868b
Keze7 <127592616+Keze7@users.noreply.github.com>
2023-06-08 10:35:32 +0800

    Create Gdong.js

---
+++ Qx/Gdong.js
@@ -0,0 +1,33 @@
+[rewrite_local]
+
+^http[s]?:\/\/gdongapi\.wo-ish\.com\/user script-response-body https://github.com/Keze7/Kaze/raw/main/Qx/Gdong.js
+
+[mitm]
+
+hostname = gdongapi.wo-ish.com
+
+/*
+
+‼️规则完全免费，仅供学习交流，🈲️商业用途
+
+*/
+
+var body = $response.body;
+
+var url = $request.url;
+
+var obj = JSON.parse(body);
+
+const Kaze = '/getUInfo';
+
+if (url.indexOf(Kaze) != -1) {
+
+    obj.data.validdate= "2099-06-10";
+
+obj.data.isvip= 1;
+
+    body = JSON.stringify(obj);
+
+}
+
+$done({body});
