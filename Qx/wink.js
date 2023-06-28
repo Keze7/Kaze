b71bd7792814e36d8f23ad5c3d3ad3c64f1cb250
Keze7 <127592616+Keze7@users.noreply.github.com>
2023-06-28 13:04:55 +0800

    更新 wink.js

--- Qx/wink.js
+++ Qx/wink.js
@@ -2,3 +2,22 @@
 ^https:\/\/api-sub\.meitu\.com\/v2\/user url script-response-body https://github.com/Keze7/Kaze/raw/main/Qx/wink.js
 [mitm]
 hostname = api-sub.meitu.com
+
+
+
+/*
+‼️规则完全免费，仅供学习交流，🈲️商业用途
+*/
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/vip_info_by_group.json';
+if (url.indexOf(vip) != -1) {
+    obj.data.is_vip = true;
+    obj.data.use_vip = true;
+
+
+    body = JSON.stringify(obj);
+}
+$done({body});
