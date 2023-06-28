2656cff6cdf3d7aecaf8099a4ebf8ddcd3f7cff0
Keze7 <127592616+Keze7@users.noreply.github.com>
2023-06-28 13:08:59 +0800

    Update Wink.js

--- Qx/Wink.js
+++ Qx/Wink.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api-sub\.meitu\.com\/v2\/user url script-response-body https://github.com/Keze7/Kaze/raw/main/Qx/wink.js
+^https:\/\/api-sub\.meitu\.com\/v2\/user url script-response-body https://github.com/Keze7/Kaze/raw/main/Qx/Wink.js
 [mitm]
 hostname = api-sub.meitu.com
 
